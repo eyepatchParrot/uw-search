e6ab521b2f78856caf1fa5bf37daa376cc4aeca0
johnchronis <johnchronis@di.uoa.gr>
2018-04-12 23:21:54 -0500

    Original makefile, fix installation script

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -1,6 +1,6 @@
 wget http://releases.llvm.org/5.0.0/clang+llvm-5.0.0-linux-x86_64-ubuntu16.04.tar.xz
 tar xvf clang+llvm-5.0.0-linux-x86_64-ubuntu16.04.tar.xz 
-mv  clang+llvm-5.0.0-linux-x86_64-ubuntu16.04 ~/clang5t
+mv  clang+llvm-5.0.0-linux-x86_64-ubuntu16.04 ~/clang5
 
 rm clang+llvm-5.0.0-linux-x86_64-ubuntu16.04.tar.xz
 sudo apt install libomp-dev
