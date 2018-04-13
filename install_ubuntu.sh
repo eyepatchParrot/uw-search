5b44b7941599b7ed6bc5e46eeb95d7757b3f0e11
yannis <yannis@node.search.quickstep-pg0.wisc.cloudlab.us>
2018-04-12 23:06:44 -0500

    clang installation script for Ubuntu 16.10 and corresponding Makefile (this can be made cleaner)

---
+++ install_ubuntu.sh
@@ -0,0 +1,8 @@
+wget http://releases.llvm.org/5.0.0/clang+llvm-5.0.0-linux-x86_64-ubuntu16.04.tar.xz
+tar xvf clang+llvm-5.0.0-linux-x86_64-ubuntu16.04.tar.xz 
+mv  clang+llvm-5.0.0-linux-x86_64-ubuntu16.04 ~/clang5t
+
+rm clang+llvm-5.0.0-linux-x86_64-ubuntu16.04.tar.xz
+sudo apt install libomp-dev
+
+
