19b0bf1d60fe7abdce57e5a99ebc7bbc11f2c672
Yannis Chronis <johnchronis@gmail.com>
2018-04-29 02:39:52 -0500

    Update install_ubuntu.sh

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -3,6 +3,16 @@ tar xvf clang+llvm-5.0.0-linux-x86_64-ubuntu16.04.tar.xz
 mv  clang+llvm-5.0.0-linux-x86_64-ubuntu16.04 ~/clang5
 
 rm clang+llvm-5.0.0-linux-x86_64-ubuntu16.04.tar.xz
-sudo apt install libomp-dev
+sudo apt install -y libomp-dev
+sudo apt-get install -y libc++-dev
+
+#wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh
+
+#chmod +x ./Miniconda2-latest-Linux-x86_64.sh
+#./Miniconda2-latest-Linux-x86_64.sh
+
+
+
+#rm Miniconda2-latest-Linux-x86_64.sh
 
 
