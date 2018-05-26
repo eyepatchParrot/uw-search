97d28e3c243f40bd021f6a17b8e11c0b8227961d
eyepatchParrot <peter.vansandt@gmail.com>
2018-05-25 22:48:26 -0500

    run.py reads from stdin now

--- run.py
+++ run.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
-from os import popen
+from os import popen 
 from subprocess import call
 from pandas import read_csv
+import sys
 
 call(['make', 'release'])
 run_param = ['algorithm', 'n', 'param', 'distribution', 'record', 'n_thds']
-df = read_csv(popen('./search i-seq.tsv'), sep='\t')
-print(df.groupby(run_param)['ns'].describe())
+df = read_csv(sys.stdin, sep='\t')
+print(df.groupby(run_param)['ns'].describe(percentiles=[.5]))
